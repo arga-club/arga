8de3044848d14230612dfe0e6dad46c9ec9aa9cc
J Garcia <j@jgarcia.jp>
2024-02-01 20:16:47 +0900

    prettierfix

--- contracts/Arga.sol
+++ contracts/Arga.sol
@@ -1,79 +1,79 @@
 // SPDX-License-Identifier: UNLICENSED
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
 
 pragma solidity ^0.8.22;
 
 contract Arga is Ownable {
-    address public feeReceiver;
+	address public feeReceiver;
 
-    constructor(address initialOwner, address initialFeeReceiver) Ownable(initialOwner) {
-        feeReceiver = initialFeeReceiver;
-    }
+	constructor(address initialOwner, address initialFeeReceiver) Ownable(initialOwner) {
+		feeReceiver = initialFeeReceiver;
+	}
 
-    struct Declaration {
-        bytes32 summary;
-        string description;
-        address actor;
-        address witness;
-        uint startDate;
-        uint endDate;
-        uint witnessByDate;
-        uint collateralValue;
-        address collateralErc20Address;
-    }
+	struct Declaration {
+		bytes32 summary;
+		string description;
+		address actor;
+		address witness;
+		uint startDate;
+		uint endDate;
+		uint witnessByDate;
+		uint collateralValue;
+		address collateralErc20Address;
+	}
 
-    Declaration[] public declarations;
-    mapping(address=>uint[]) public actorDeclarationsActive;
-    mapping(address=>uint[]) public actorDeclarations;
-    mapping(address=>uint[]) public witnessDeclarations;
-    mapping(address=>uint[]) public witnessDeclarationsActive;
+	Declaration[] public declarations;
+	mapping(address => uint[]) public actorDeclarationsActive;
+	mapping(address => uint[]) public actorDeclarations;
+	mapping(address => uint[]) public witnessDeclarations;
+	mapping(address => uint[]) public witnessDeclarationsActive;
 
-    event DeclarationMade(Declaration declaration);
-    event DeclarationConcluded(Declaration declaration);
+	event DeclarationMade(Declaration declaration);
+	event DeclarationConcluded(Declaration declaration);
 
-    function declareWithEther(
-        bytes32 summary,
-        string memory description,
-        address witness,
-        uint startDate,
-        uint endDate,
-        uint witnessByDate
-    ) public payable {
-        Declaration memory declaration = Declaration(
-            summary,
-            description,
-            msg.sender, // actor
-            witness,
-            startDate,
-            endDate,
-            witnessByDate,
-            msg.value,
-            address(0)
-        );
-        emit DeclarationMade(declaration);
-    }
-    function declareWithToken(
-        bytes32 summary,
-        string memory description,
-        address witness,
-        uint startDate,
-        uint endDate,
-        uint witnessByDate,
-        uint collateralValue,
-        address collateralErc20Address
-    ) public {
-        Declaration memory declaration = Declaration(
-            summary,
-            description,
-            msg.sender, // actor
-            witness,
-            startDate,
-            endDate,
-            witnessByDate,
-            collateralValue,
-            collateralErc20Address
-        );
-        emit DeclarationMade(declaration);
-    }
+	function declareWithEther(
+		bytes32 summary,
+		string memory description,
+		address witness,
+		uint startDate,
+		uint endDate,
+		uint witnessByDate
+	) public payable {
+		Declaration memory declaration = Declaration(
+			summary,
+			description,
+			msg.sender, // actor
+			witness,
+			startDate,
+			endDate,
+			witnessByDate,
+			msg.value,
+			address(0)
+		);
+		emit DeclarationMade(declaration);
+	}
+	function declareWithToken(
+		bytes32 summary,
+		string memory description,
+		address witness,
+		uint startDate,
+		uint endDate,
+		uint witnessByDate,
+		uint collateralValue,
+		address collateralErc20Address
+	) public {
+		Declaration memory declaration = Declaration(
+			summary,
+			description,
+			msg.sender, // actor
+			witness,
+			startDate,
+			endDate,
+			witnessByDate,
+			collateralValue,
+			collateralErc20Address
+		);
+		emit DeclarationMade(declaration);
+	}
 }
