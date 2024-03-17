ace21b048b1f87779f4ab0fb1470cbb41f72c9af
J Garcia <j@jgarcia.jp>
2024-03-17 22:14:43 +0900

    add solidity plugin in prettierrc

---
+++ core/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	trailingComma: "all",
+	tabWidth: 3,
+	semi: false,
+	singleQuote: true,
+	useTabs: true,
+	printWidth: 120,
+	arrowParens: "avoid",
+	plugins: ['prettier-plugin-solidity']
+}
