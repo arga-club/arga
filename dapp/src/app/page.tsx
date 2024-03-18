aa2f75508d49bc20aaa071b87f4380b5682c92b6
J Garcia <j@jgarcia.jp>
2024-03-18 22:05:39 +0900

    load declarations for account

--- dapp/src/app/page.tsx
+++ dapp/src/app/page.tsx
@@ -3,49 +3,51 @@
 import Link from 'next/link'
 import styled from 'styled-components'
 import tw from 'twin.macro'
-import { useReadContract } from 'wagmi'
+import { useAccount, useReadContract } from 'wagmi'
 import { hardhat } from 'wagmi/chains'
 import { Button } from '~/app/_components/ui/button'
-import { Card, CardContent, CardFooter } from '~/app/_components/ui/card'
+import { Card, CardContent } from '~/app/_components/ui/card'
 import { argaAbi } from '~/lib/generated'
 
 export default function Home() {
-	const { isInitialLoading, data } = useReadContract({
+	const { address, isConnecting } = useAccount()
+
+	const { isInitialLoading, data: actorDeclarations } = useReadContract({
+		query: { enabled: !!address },
 		abi: argaAbi,
 		address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
 		functionName: 'actorDeclarations',
-		args: ['0x70997970C51812dc3A010C7d01b50e0d17dc79C8'],
+		args: address ? [address] : undefined,
 		chainId: hardhat.id,
 	})
+
 	return (
 		<div tw='container'>
-			<div tw='px-3 pt-16'>
+			<div tw='px-3 pt-16 pb-20 space-y-10'>
 				<PageHeading>Declarations</PageHeading>
-				<Card>
-					<CardContent className='pt-8'>
-						{isInitialLoading
-							? 'Loading..'
-							: !data?.length
+				<div>
+					{isInitialLoading
+						? 'Loading..'
+						: isConnecting
+							? 'Connecting..'
+							: !actorDeclarations?.length
 								? 'No declarations..'
-								: data?.map(declaration => {
+								: actorDeclarations?.map(declaration => {
 										return (
 											<Card key={declaration.id}>
 												<CardContent>{declaration.summary}</CardContent>
 											</Card>
 										)
 									})}
-					</CardContent>
-					<CardFooter>
-						<Link href='/declaration/new'>
-							<Button>New Declaration</Button>
-						</Link>
-					</CardFooter>
-				</Card>
+				</div>
+				<Link tw='block' href='/declaration/new'>
+					<Button>New Declaration</Button>
+				</Link>
 			</div>
 		</div>
 	)
 }
 
 const PageHeading = styled.h1`
-	${tw`text-3xl pb-10`}
+	${tw`text-3xl`}
 `
