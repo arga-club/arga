c79495cccd699231bef98fee08a173dc2c2950c3
J Garcia <j@jgarcia.jp>
2024-04-02 23:52:52 +0900

    favicon

--- dapp/src/app/declarations/layout.tsx
+++ dapp/src/app/declarations/layout.tsx
@@ -17,7 +17,17 @@ const inter = Inter({
 export const metadata = {
 	title: 'Arga',
 	description: 'Declare',
-	icons: [{ rel: 'icon', url: '/favicon.ico' }],
+	icons: [
+		{ rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon-16x16.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon-32x32.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '48x48', url: '/favicon-48x48.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '64x64', url: '/favicon-64x64.png' },
+		{ rel: 'apple-touch-icon', sizes: '180x180', url: '/favicon-180x180.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '192x192', url: '/favicon-192x192.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '128x128', url: '/favicon-128x128.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '152x152', url: '/favicon-152x152.png' },
+		{ rel: 'icon', type: 'image/png', sizes: '167x167', url: '/favicon-167x167.png' },
+	],
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
