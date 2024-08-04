b6b2e2253d65bb14bb9fcc2567cc1c681fc61cb4
J Garcia <j@jgarcia.jp>
2024-08-04 22:44:45 +0900

    fork OP sepolia

--- core/hardhat.config.dev.ts
+++ core/hardhat.config.dev.ts
@@ -21,6 +21,9 @@ export default {
 			},
 		],
 	},
+	// mocha: {
+	// 	timeout: ms('10m'),
+	// },
 	diamondAbi: {
 		name: 'ArgaDiamond',
 		include: ['DeclarationFacet', 'PoolFacet', 'RedemptionFacet', 'TreasuryFacet', 'OwnershipFacet'],
@@ -28,10 +31,17 @@ export default {
 	},
 	networks: {
 		hardhat: {
-			mining: {
-				auto: false,
-				interval: 5000,
+			forking: {
+				url: 'https://opt-sepolia.g.alchemy.com/v2/SCkfInlsYCGj1mnX4rj-jdKEUxx-t92X',
+				blockNumber: 15476319,
 			},
+			// mining: {
+			// 	auto: false,
+			// 	interval: 5000,
+			// },
+			gas: 12e6,
+			gasPrice: 8e9,
+			blockGasLimit: 20e6,
 		},
 	},
 	ethernal: {
