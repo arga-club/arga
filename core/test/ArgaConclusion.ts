4bfec1ade412c56775681d1e67092d5813523926
J Garcia <j@jgarcia.jp>
2024-08-11 15:47:36 +0900

    remove debugging code from test

--- core/test/ArgaConclusion.ts
+++ core/test/ArgaConclusion.ts
@@ -166,61 +166,31 @@ describe('Conclusion', function () {
 			const contractBalance = await owner.provider.getBalance(arga)
 			expect(contractBalance).to.equal(0n)
 		})
-		it.only('wins pool when multiplier is high enough', async () => {
+		it('wins pool when multiplier is high enough', async () => {
 			const { arga, actor, witness, owner } = await loadFixture(fixture)
-			console.log('makeDeclaration')
 
 			// create a rejected declaration so that pool will have collateral
 			await arga.connect(owner).changeWinMultiplier(0)
 			const {
 				expectedDeclaration: [id],
 			} = await makeDeclaration({ arga, actor, witness })
-			{
-				const balance = await owner.provider.getBalance(arga)
-				console.log({ balance })
-			}
-			console.log('submitDeclarationProof')
 			await submitDeclarationProof({ arga, actor, witness })
-			console.log('concludeDeclarationWithRejection')
 			await arga.connect(witness).concludeDeclarationWithRejection(id, randomNumberForDraw())
 			const poolAmount = (value * 96n) / 100n
 			expect(await arga.pool()).to.deep.equal([[poolAmount, hre.ethers.ZeroAddress]])
 			expect(await arga.redemptionsForParty(actor)).to.deep.equal([])
-			const treasurerRedemptions = await arga.redemptionsForParty(owner)
-			console.log({ treasurerRedemptions })
 
 			// win pool
 			await makeDeclaration({ arga, actor, witness })
-			{
-				const balance = await owner.provider.getBalance(arga)
-				console.log({ balance })
-			}
 			await arga.connect(actor).submitDeclarationProof(1n, proof)
 			await arga.connect(owner).changeWinMultiplier(50)
 			const randomNumber = randomNumberForDraw()
 			await arga.connect(witness).concludeDeclarationWithApproval(1n, randomNumber)
 
 			// provide entropy value
-			// await transaction.wait()
-			// const logs = receipt?.logs
 			const entropyContractAddress = '0x4821932D0CDd71225A6d914706A621e0389D7061'
-			// const entropyContract = await hre.ethers.getContractAt('Entropy', entropyContractAddress)
-			// const events = logs?.map(
-			// 	log => arga.interface.parseLog(log as any) ?? entropyContract.interface.parseLog(log as any),
-			// )
-			// console.log('events?.[1]?.args', events?.[1]?.args)
-			// console.log({ events })
-			const { drawId } = await arga.getDeclaration(1n)
-			// console.log({ drawId })
-			// const pendingDraw = await arga.draw(drawId)
-			// console.log({ pendingDraw })
-			// await entropyContract.revealWithCallback(
-			// 	'0x6CC14824Ea2918f5De5C2f75A9Da968ad4BD6344',
-			// 	drawId,
-			// 	randomNumber,
-			// 	randomNumberForDraw(),
-			// )
 			const entropyProviderAddress = '0x6CC14824Ea2918f5De5C2f75A9Da968ad4BD6344'
+			const { drawId } = await arga.getDeclaration(1n)
 			await hre.network.provider.request({
 				method: 'hardhat_impersonateAccount',
 				params: [entropyContractAddress],
@@ -231,15 +201,6 @@ describe('Conclusion', function () {
 			])
 			const providerSigner = await hre.ethers.getSigner(entropyContractAddress)
 			await arga.connect(providerSigner)._entropyCallback(drawId, entropyProviderAddress, randomNumberForDraw())
-			{
-				const balance = await owner.provider.getBalance(arga)
-				console.log({ balance })
-			}
-
-			// const updatedDeclaration = await arga.getDeclaration(id)
-			// console.log({ updatedDeclaration })
-			const updatedDraw = await arga.draw(drawId)
-			console.log({ updatedDraw })
 
 			const actorRedemption = (value * 96n) / 100n
 			expect(await arga.redemptionsForParty(actor)).to.deep.equal([
@@ -252,15 +213,7 @@ describe('Conclusion', function () {
 				const balanceAfter = await actor.provider.getBalance(actor.address)
 				expect(balanceAfter).to.equal(balanceBefore - gasUsed + poolAmount + actorRedemption)
 			}
-			{
-				const balance = await owner.provider.getBalance(arga)
-				console.log({ balance })
-			}
 			await arga.connect(witness).redeem(witness.address, [hre.ethers.ZeroAddress])
-			{
-				const balance = await owner.provider.getBalance(arga)
-				console.log({ balance })
-			}
 			await arga.connect(owner).redeem(owner.address, [hre.ethers.ZeroAddress])
 			const contractBalance = await owner.provider.getBalance(arga)
 			expect(contractBalance).to.equal(0n)
