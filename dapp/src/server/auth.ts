8dff3f2f9f14c9f7c1f8906328eda489006d51dd
J Garcia <j@werks.co.jp>
2024-10-09 00:28:40 +0900

    configure auth

--- dapp/src/server/auth.ts
+++ dapp/src/server/auth.ts
@@ -2,8 +2,11 @@ import { PrismaAdapter } from '@auth/prisma-adapter'
 import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth'
 import { type Adapter } from 'next-auth/adapters'
 import DiscordProvider from 'next-auth/providers/discord'
+import { compare } from 'bcryptjs'
+import CredentialsProvider from 'next-auth/providers/credentials'
 import { env } from '~/env'
 import { db } from '~/server/db'
+import { type FIXME } from '~/types/utils'
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -32,16 +35,13 @@ declare module 'next-auth' {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
-	callbacks: {
-		session: ({ session, user }) => ({
-			...session,
-			user: {
-				...session.user,
-				id: user.id,
-			},
-		}),
-	},
 	adapter: PrismaAdapter(db) as Adapter,
+	pages: {
+		signIn: '/login',
+	},
+	session: {
+		strategy: 'jwt',
+	},
 	providers: [
 		DiscordProvider({
 			clientId: env.DISCORD_CLIENT_ID,
@@ -56,7 +56,55 @@ export const authOptions: NextAuthOptions = {
 		 *
 		 * @see https://next-auth.js.org/providers/github
 		 */
+		CredentialsProvider({
+			name: 'Sign in',
+			credentials: {
+				email: { label: 'Email', type: 'email' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials.password) {
+					return null
+				}
+
+				const user = await db.user.findUnique({
+					where: {
+						email: credentials.email,
+					},
+				})
+
+				if (!user?.password || !(await compare(credentials.password, user.password))) {
+					return null
+				}
+
+				return {
+					id: user.id,
+					email: user.email,
+				}
+			},
+		}),
 	],
+	callbacks: {
+		session: ({ session, token }) => {
+			return {
+				...session,
+				user: {
+					email: session.user.email,
+					id: token.id,
+				},
+			}
+		},
+		jwt: ({ token, user }) => {
+			if (user) {
+				const u = user as unknown as FIXME
+				return {
+					email: token.email,
+					id: u.id,
+				}
+			}
+			return token
+		},
+	},
 }
 
 /**
