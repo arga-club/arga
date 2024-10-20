85423e5edf5b5cbf7a5133c1b28c458b1430dbe6
J Garcia <j@jgarcia.jp>
2024-10-20 22:42:39 +0900

    add pool to declarations layout

--- dapp/src/app/_components/layouts/SidebarLayout.tsx
+++ dapp/src/app/_components/layouts/SidebarLayout.tsx
@@ -35,11 +35,19 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from '~/app/_components/ui/dropdown-menu'
+import { Card, CardContent } from '~/app/_components/ui/card'
+import { Prose } from '~/app/_components/ui/prose'
+import { normalizeBigJSON } from '~/lib/ethereum-utils'
+import { LazyReactJSON } from '~/lib/react-utils'
+import { useReadArgaDiamondPool } from '~/lib/generated'
+import { chainId } from '~/lib/wagmi-config'
+import { Button } from '~/app/_components/ui/button'
 
 export const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
 	const { data: session } = useSession()
 	const user = session?.user
 	const { isMobile } = useSidebar()
+	const { data: pool } = useReadArgaDiamondPool({ chainId })
 	return (
 		<div tw='container'>
 			<div tw='md:flex md:items-start'>
@@ -184,8 +192,36 @@ export const SidebarLayout = ({ children }: { children: React.ReactNode }) => {
 						)}
 					</Sidebar>
 				</div>
-				<div tw='md:w-4/5 py-24 pl-4'>
+				<div tw='md:w-3/5 py-24 pl-4'>
 					<div tw='pt-2'>{children}</div>
+				</div>
+				<div tw='md:w-1/5 py-24 pl-4'>
+					<div tw='pt-2'>
+						<div tw='space-y-10'>
+							<Prose>
+								<h1>Pool</h1>
+								<p>
+									When a declaration is rejected by its witness, its collateral is sent to this pool instead of
+									returned to the user. All actors concluding a declaration when assets are in the pool have a
+									chance to win those assets.
+								</p>
+							</Prose>
+							<Card className='max-w-screen-sm'>
+								{!pool?.length ? (
+									<CardContent className='space-y-6 pt-8'>
+										<Prose>0 ETH</Prose>
+									</CardContent>
+								) : (
+									<CardContent className='space-y-6 pt-8'>
+										<LazyReactJSON src={normalizeBigJSON(pool)} name='pool' />
+									</CardContent>
+								)}
+							</Card>
+							<Link tw='block' href='/declarations/new'>
+								<Button>New Declaration</Button>
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
