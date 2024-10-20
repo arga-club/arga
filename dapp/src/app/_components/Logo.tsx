e684916ed576b2303f495f5443d947cc963d0502
J Garcia <j@jgarcia.jp>
2024-10-20 11:49:27 +0900

    logo

--- dapp/src/app/_components/Logo.tsx
+++ dapp/src/app/_components/Logo.tsx
@@ -2,13 +2,13 @@
 
 import Image from 'next/image'
 import { Prose } from '~/app/_components/ui/prose'
-import logo from '~/images/abstract-option-08.png'
+import logo from '~/images/abstract-options-07-icon.png'
 
 export const Logo = ({ className }: { className?: string }) => {
 	return (
 		<Prose className={className} tw='flex space-x-4 items-center'>
-			<Image src={logo} alt='flowchart showing process in detail' tw='w-16 h-16 my-0' />
-			<h1 tw='mb-0 text-5xl'>Arga</h1>
+			<Image src={logo} alt='flowchart showing process in detail' tw='size-12 my-0' />
+			<h1 tw='mb-0 text-4xl translate-y-[-1px]'>Arga</h1>
 		</Prose>
 	)
 }
