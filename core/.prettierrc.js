667410f863353bcac67a8c4ecc63d89168a4233a
J Garcia <j@jgarcia.jp>
2024-10-28 23:55:42 +0900

    prettier:fix

--- core/.prettierrc.js
+++ core/.prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-	trailingComma: "all",
+	trailingComma: 'all',
 	tabWidth: 3,
 	semi: false,
 	singleQuote: true,
 	useTabs: true,
 	printWidth: 120,
-	arrowParens: "avoid",
-	plugins: ['prettier-plugin-solidity']
+	arrowParens: 'avoid',
+	plugins: ['prettier-plugin-solidity'],
 }
