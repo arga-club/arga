7c1e54de3dcd9731217f44ff6a32419236383386
J Garcia <j@jgarcia.jp>
2024-10-29 00:06:00 +0900

    tweak line endings

--- core/.prettierrc.js
+++ core/.prettierrc.js
@@ -7,4 +7,18 @@ module.exports = {
 	printWidth: 120,
 	arrowParens: 'avoid',
 	plugins: ['prettier-plugin-solidity'],
+	overrides: [
+		{
+			files: [
+				'contracts/upgradeInitializers/DiamondInit.sol',
+				'contracts/interfaces/IDiamondLoupe.sol',
+				'contracts/facets-arga/TreasuryFacet.sol',
+				'contracts/facets-arga/DeclarationFacet.sol',
+				'contracts/Arga.sol',
+			],
+			options: {
+				endOfLine: 'crlf',
+			},
+		},
+	],
 }
